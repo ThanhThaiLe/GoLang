97d6c16d075a6c7c6d2f578fc00f9d82e4c99315
ThanhThaiLe <92001899+ThanhThaiLe@users.noreply.github.com>
2023-12-06 13:52:30 +0700

    01.variable

---
+++ 01.variable/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	//cú pháp
+	//var + variable name + datatype + value
+	var name string = "Lê Thanh Thái"
+	fmt.Println(name)
+
+	var first_name, last_name string = "Lê", " Thanh Thái"
+	fmt.Println(first_name + last_name)
+
+	var a int = 5
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+	fmt.Println(b + c)
+
+	// biến hiểu ngầm với giá trị khởi tạo
+
+	var number = 5
+	fmt.Println(number)
+	fmt.Printf("%T: %d\n", number, number)
+	fmt.Println("Demo")
+	x := 42
+	y := float32(43.3)
+	z := "hello"
+
+	xt := reflect.TypeOf(x).Kind()
+	yt := reflect.TypeOf(y).Kind()
+	zt := reflect.TypeOf(z).Kind()
+
+	fmt.Printf("%T: %s\n", xt, xt)
+	fmt.Printf("%T: %s\n", yt, yt)
+	fmt.Printf("%T: %s\n", zt, zt)
+
+	if xt == reflect.Int {
+		println(">> x is int")
+	}
+	if yt == reflect.Float32 {
+		println(">> y is float32")
+	}
+	if zt == reflect.String {
+		println(">> z is string")
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # GoLang
+
 GoLang
+
+-- init go
+=> go mod init name-file
+-- create main.go
+New-Item -ItemType File -Name main.go
